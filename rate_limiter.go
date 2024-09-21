764cfb264b9ae88b5823ca3725ef505b50bdb7ac
Max Malekzadeh <mxmlkzdh@gmail.com>
2024-09-21 16:01:09 -0400

    Add the RateLimiter interface.

---
+++ rate_limiter.go
@@ -0,0 +1,17 @@
+package cerberus
+
+import (
+	"net/http"
+)
+
+// RateLimiter defines an interface for controlling the rate of requests
+// to a resource or service. It provides a method to check if a request
+// is allowed to proceed based on the implemented rate limiting logic.
+type RateLimiter interface {
+	// IsAllowed checks whether a request is permitted to proceed based on the
+	// current rate limiting rules. It returns true if the request
+	// is allowed, false otherwise. An error may be returned if
+	// there are issues with the underlying rate limiting logic,
+	// such as connectivity to a backend service, a cache server, or configuration errors.
+	IsAllowed(req *http.Request) (bool, error)
+}
